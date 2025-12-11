916d898296229f3447b6653770d419e8c8dfd528
Matteo <217795844+matteocolombo-dev@users.noreply.github.com>
2025-12-11 13:00:18 +0100

    Aggiunto funzionalità passo in minuti e in secondi e modifica al layout

--- docs/script.js
+++ docs/script.js
@@ -5,6 +5,9 @@ const pace = document.getElementById("pace");
 const form = document.getElementById("form");
 const resultMessage = document.getElementById("result-message");
 const message = document.getElementById("result");
+const paceMin = document.getElementById("pace-min");
+const paceSec = document.getElementById("pace-sec");
+
 
 
 function isValidInput (input) {
@@ -19,8 +22,8 @@ function isValidInput (input) {
 function calcNumGelToBring () {
 
     //Tempo di allenamento stimato in ore in base al passo
-    const totTimeWorkoutHour = ((workoutKm.value * pace.value) / 60).toFixed(2); 
-    console.log(totTimeWorkoutHour);
+    const totTimeWorkoutHour = (((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60)) / 60).toFixed(2); 
+    console.log(totTimeWorkoutHour)
 
     //Numero di carboidrati assunti per tutto l'allenamento
     const totCarbWorkout = strategy.value * totTimeWorkoutHour;
@@ -36,7 +39,7 @@ function calcNumGelToBring () {
 function calcMin () {
 
     //Tempo di allenamento stimato in minuti in base al passo
-    const totTimeWorkoutMin = workoutKm.value * pace.value;
+    const totTimeWorkoutMin = ((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60));
 
     // Ogni quanti minuti assumere il cibo?
     const min = (totTimeWorkoutMin / calcNumGelToBring ()).toFixed(2);
@@ -46,13 +49,18 @@ function calcMin () {
 
 function showResult () {
     message.textContent = "";
-    message.textContent = `You need to bring ${calcNumGelToBring() - 1} and take them every ${calcMin()} minutes`;
+    message.textContent = `You need to bring ${(calcNumGelToBring() - 1).toFixed(2)} and take them every ${calcMin()} minutes`;
     resultMessage.style.display = "block";    
 }
 
 form.addEventListener("submit",(e)=> {
     e.preventDefault();
     showResult();
+    strategy.value= "";
+    carbPerFoods.value="";
+    workoutKm.value = "";
+    paceMin.value = "";
+    paceSec.value = "";
 });
 
 
