41d3eafccb1021b16d0cc417e4e18e1d819d8e62
Matteo <217795844+matteocolombo-dev@users.noreply.github.com>
2025-12-18 12:57:15 +0100

    Aggiunto tempo totale di allenamento, totale carbs, risolto bugs, modificato estetica

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,4 @@
+const main = document.querySelector("main")
 const strategy = document.getElementById("carb-per-hour");
 const carbPerFoods = document.getElementById("carb-per-foods");
 const workoutKm = document.getElementById("workout-km");
@@ -7,8 +8,8 @@ const resultMessage = document.getElementById("result-message");
 const message = document.getElementById("result");
 const paceMin = document.getElementById("pace-min");
 const paceSec = document.getElementById("pace-sec");
-
-
+const totalTimeWorkout = document.getElementById("total-time-workout");
+const totalCarbohidrates = document.getElementById("total-carbohidrates");
 
 function isValidInput (input) {
 
@@ -40,6 +41,7 @@ function calcMin () {
 
     //Tempo di allenamento stimato in minuti in base al passo
     const totTimeWorkoutMin = ((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60));
+    console.log("Tempo totale di allenamento:",totTimeWorkoutMin)
 
     // Ogni quanti minuti assumere il cibo?
     const min = (totTimeWorkoutMin / calcNumGelToBring ()).toFixed(2);
@@ -47,7 +49,24 @@ function calcMin () {
     return min;
 }
 
+function calcTotalTimeWorkout () {
+    let hour = 0;
+
+    let totalTimeWorkoutMin = ((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60))
+
+    while (totalTimeWorkoutMin >= 60) {
+        totalTimeWorkoutMin = totalTimeWorkoutMin - 60
+        hour += 1;
+        
+    }
+
+    return `${hour} hour ${Math.round(totalTimeWorkoutMin)} min`
+    
+}
+
 function showResult () {
+    totalTimeWorkout.textContent = ` ${calcTotalTimeWorkout ()}`;
+    totalCarbohidrates.textContent = ` ${Math.round(strategy.value * (((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60)) / 60))} grams`;
     message.textContent = "";
     message.textContent = `You need to bring ${(calcNumGelToBring() - 1).toFixed(2)} and take them every ${calcMin()} minutes`;
     resultMessage.style.display = "block";    
@@ -56,11 +75,12 @@ function showResult () {
 form.addEventListener("submit",(e)=> {
     e.preventDefault();
     showResult();
-    strategy.value= "";
-    carbPerFoods.value="";
-    workoutKm.value = "";
-    paceMin.value = "";
-    paceSec.value = "";
+    // strategy.value= "";
+    // carbPerFoods.value="";
+    // workoutKm.value = "";
+    // paceMin.value = "";
+    // paceSec.value = "";
+    main.style.height = "730px"
 });
 
 
