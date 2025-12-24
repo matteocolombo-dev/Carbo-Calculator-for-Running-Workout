728850ecb99cf44e8f5a6f5131e5b474a951cb05
Matteo <217795844+matteocolombo-dev@users.noreply.github.com>
2025-12-24 13:21:24 +0100

    Aggiunto funzionalità Validità dell'input, mostra/nascondi bottoni calcola e ricalcola, event.keydown sul bottone ricalcola. Pulizia codice Javascript ripetitivo

--- docs/script.js
+++ docs/script.js
@@ -10,78 +10,109 @@ const paceMin = document.getElementById("pace-min");
 const paceSec = document.getElementById("pace-sec");
 const totalTimeWorkout = document.getElementById("total-time-workout");
 const totalCarbohidrates = document.getElementById("total-carbohidrates");
+const calculateBtn = document.getElementById("calculate-btn");
+const resetBtn = document.getElementById("reset-btn");
 
-function isValidInput (input) {
 
-    const regex = /\d+e\d+/i;
+function isValidInput () {
+    const numberRegex =  /^(?:[1-9]\d*|0)(?:[.,]\d+)?$/
+    const inputs = [strategy.value, carbPerFoods.value, workoutKm.value, paceMin.value, paceSec.value];
 
-    if (input < 0 || regex.test(input)) 
-    return true;
+    const isValid = inputs.every((input) => numberRegex.test(input))
+    return isValid
 };
+
+// Funzione che calcola il tempo totale dell'allenamento in minuti
+
+function totalTimeWorkoutMin () {
+     const totTimeWorkoutMin = ((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60));
+     return totTimeWorkoutMin;
+}
 
 
 function calcNumGelToBring () {
 
     //Tempo di allenamento stimato in ore in base al passo
-    const totTimeWorkoutHour = (((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60)) / 60).toFixed(2); 
-    console.log(totTimeWorkoutHour)
+    const totTimeWorkoutHour = (totalTimeWorkoutMin() / 60).toFixed(2); 
 
     //Numero di carboidrati assunti per tutto l'allenamento
     const totCarbWorkout = strategy.value * totTimeWorkoutHour;
-    console.log(totCarbWorkout);
 
     // Numero di gel, barrette o altri cibi carb da portare
-    const numberFoods = (totCarbWorkout / carbPerFoods.value).toFixed(2);
-    console.log(numberFoods);
-
-    return numberFoods;    
+    const numberOfFoods = (totCarbWorkout / carbPerFoods.value).toFixed(2);
+    return numberOfFoods;    
 }
 
-function calcMin () {
+function calcMinForGel () {
 
     //Tempo di allenamento stimato in minuti in base al passo
-    const totTimeWorkoutMin = ((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60));
-    console.log("Tempo totale di allenamento:",totTimeWorkoutMin)
+    const timeMin = totalTimeWorkoutMin ();
 
     // Ogni quanti minuti assumere il cibo?
-    const min = (totTimeWorkoutMin / calcNumGelToBring ()).toFixed(2);
+    const minForgel = (timeMin / calcNumGelToBring ()).toFixed(2);
 
-    return min;
+    return minForgel;
 }
 
-function calcTotalTimeWorkout () {
+function stringTotalTimeWorout () {
     let hour = 0;
 
-    let totalTimeWorkoutMin = ((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60))
+    let timeMin = totalTimeWorkoutMin()
 
-    while (totalTimeWorkoutMin >= 60) {
-        totalTimeWorkoutMin = totalTimeWorkoutMin - 60
-        hour += 1;
-        
+    while (timeMin >= 60) {
+        timeMin = timeMin - 60
+        hour += 1;      
     }
 
-    return `${hour} hour ${Math.round(totalTimeWorkoutMin)} min`
-    
+    return `${hour} hour ${Math.round(timeMin)} min` 
 }
 
 function showResult () {
-    totalTimeWorkout.textContent = ` ${calcTotalTimeWorkout ()}`;
-    totalCarbohidrates.textContent = ` ${Math.round(strategy.value * (((workoutKm.value * paceMin.value) + (workoutKm.value * paceSec.value / 60)) / 60))} grams`;
+    totalTimeWorkout.textContent = ` ${stringTotalTimeWorout()}`;
+    totalCarbohidrates.textContent = ` ${Math.round(strategy.value * ((totalTimeWorkoutMin()) / 60))} grams`;
     message.textContent = "";
-    message.textContent = `You need to bring ${(calcNumGelToBring() - 1).toFixed(2)} and take them every ${calcMin()} minutes`;
+    message.textContent = `You need to bring ${(calcNumGelToBring() - 1).toFixed(2)} and take them every ${calcMinForGel()} minutes`;
     resultMessage.style.display = "block";    
 }
 
+function manageKeyDown (e) {
+    if(e.key === "Enter") resetCalc();
+    window.removeEventListener("keydown", manageKeyDown)
+}
+
 form.addEventListener("submit",(e)=> {
+    if (!isValidInput()) {
+        alert("You should put a valid input");
+        // e.preventDefault();
+        return;
+    };
     e.preventDefault();
     showResult();
-    // strategy.value= "";
-    // carbPerFoods.value="";
-    // workoutKm.value = "";
-    // paceMin.value = "";
-    // paceSec.value = "";
-    main.style.height = "730px"
+    main.style.height = "730px";
+    calculateBtn.style.display = "none";
+    resetBtn.style.display = "block";
+
+    window.addEventListener("keydown", manageKeyDown)
 });
+
+
+function resetCalc () {
+    resetBtn.style.display = "none"
+    resultMessage.style.display = "none"
+    calculateBtn.style.display = "block"
+    main.style.height = "600px"
+    strategy.value = "";
+    carbPerFoods.value = "";
+    workoutKm.value = "";
+    paceMin.value = "";
+    paceSec.value = "";
+}
+
+
+//OK
+resetBtn.addEventListener("click", resetCalc);
+
+
 
 
 
